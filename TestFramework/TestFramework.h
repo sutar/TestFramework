1bf7cecab7a3f6529fb373fd4777e427988cdd8a
Xin Wang <i@wangx.in>
2016-07-14 21:11:32 +0800

    Shared scheme

--- TestFramework/TestFramework.h
+++ TestFramework/TestFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
 
-
+#import <TestFramework/TestClass.h>
